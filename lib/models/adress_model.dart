43b4f7ec9b7a366fdf4db9e65a5b7b0ac6f9a4b3
minme <minme.develop@gmail.com>
2022-07-04 14:31:11 +0900

    Add error handling

--- lib/models/adress_model.dart
+++ lib/models/adress_model.dart
@@ -5,4 +5,11 @@ class AddressModel {
   late final String line1;
 
   AddressModel(this.zipCode, this.prefecture, this.city, this.line1);
+
+  AddressModel.fromJson(Map<String, String> map) {
+    this.zipCode = 'test';
+    this.prefecture = 'pref';
+    this.city = 'test';
+    this.line1 = 'test';
+  }
 }
--- lib/services/webservice.dart
+++ lib/services/webservice.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:ffi';
+
 import 'package:postalcode_test/models/adress_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,10 +12,25 @@ class WebService {
     final response = await http.get(Uri.parse(url));
     print(response);
     if (response.statusCode == 200) {
-      final json = response.body;
+      final jsonRaw = response.body;
       // 取得できたJSON形式の文字列を、AddressModelに変換する処理
-      print(json);
-      return AddressModel("123", "Test", "test", "test");
+      print(jsonRaw);
+      // dartで扱うため、Mapの形に変換する。
+      final json = jsonDecode(jsonRaw) as Map<String, dynamic>;
+      final jsonResults = json['results'];
+      final results = <Map<String, String>>[];
+      if (jsonResults is List) {
+        for (final result in jsonResults) {
+          if (result is Map<String, String>) {
+            results.add(result);
+          }
+        }
+      } else {
+        throw Exception('type error');
+      }
+      print(results[0]['address1']);
+      final result = AddressModel.fromJson(results[0]);
+      return result;
     } else {
       throw Exception('Error');
     }
